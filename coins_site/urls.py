d405f0e10141312e1b8b2e0220194da63bd71124
inframercury <inframercury@gmail.com>
2014-11-19 20:16:53 +1000

    Add bootstrap and some other files

--- coins_site/urls.py
+++ coins_site/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from coins import views
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'coins_site.views.home', name='home'),
+    url(r'^$',views.index , name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
