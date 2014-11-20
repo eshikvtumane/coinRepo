16aa51a30f7c92558f12299a9dcac957c0db6184
inframercury <inframercury@gmail.com>
2014-11-20 12:26:47 +1000

    Add base.html template, change settings

--- coins/urls.py
+++ coins/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
+from coins import views
 urlpatterns = patterns('',
     # Examples:
-    url(r'^coins/',include('coins.urls')),
+    url(r'^$',views.index , name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+
 )
