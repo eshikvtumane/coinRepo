ed450980e75a6ef11396a5b6e72c2c9b2ecb05b6
eshik <eshikvtumanebest@gmail.com>
2014-11-20 21:08:54 +1100

    Change models coins (add inherit)

--- users/forms.py
+++ users/forms.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
