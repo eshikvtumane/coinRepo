bc1c46cde6321a064335ecfecd8d852fa8fc0a73
inframercury <inframercury@gmail.com>
2014-11-21 15:05:07 +1000

    Add models to admin.py

---
+++ coins/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from coins.models import Countries,Series,Coins,CoinToMint,Metals,Mints,Denominals
+
+class CountriesAdmin(admin.ModelAdmin):
+    fields = ["country_name","country_flag"]
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    fields = ["series_name","country"]
+
+class CoinsAdmin(admin.ModelAdmin):
+    fields = ["country","series","coin_metal","denominal_coin","denominal_name","manufacture_date","coin_circulation",
+             "coin_weight","coin_diameter","coin_thickness","painter","sculptor","coin_herd","description","item_number",
+              "photo_obverse","photo_reverse","link_cbr"]
+
+class CoinToMintAdmin(admin.ModelAdmin):
+    fields = ["coin","mint"]
+
+class MetalsAdmin(admin.ModelAdmin):
+    fields = ["metal_description"]
+
+class MintsAdmin(admin.ModelAdmin):
+    fields = ["country","mint_name","mint_abbreviation"]
+class DenominalsAdmin(admin.ModelAdmin):
+    fields = ["denominal_name","denominal_country"]
+
+admin.site.register(Countries,CountriesAdmin)
+admin.site.register(Series,SeriesAdmin)
+admin.site.register(Coins,CoinsAdmin)
+admin.site.register(CoinToMint,CoinToMintAdmin)
+admin.site.register(Metals,MetalsAdmin)
+admin.site.register(Mints,MintsAdmin)
+admin.site.register(Denominals,DenominalsAdmin)
