93aaadb1d478c6d321fbc0f5145d57c82f2227bd
eshik <eshikvtumanebest@gmail.com>
2014-11-29 22:01:59 +1100

    change models coins

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
@@ -9,8 +10,8 @@ class Countries(models.Model):
 		db_table = 'Countries' # name table in DB
 
 	#id = models.AutoField(primary_key=True) # id
-	country_name = models.CharField(max_length=200)
-	country_flag = models.ImageField() # Image flag for country 
+	country_name = models.CharField(max_length='200')
+	country_flag = models.ImageField(upload_to='/static/flag', blank=True) # Image flag for country
 
 
 # http://tinyurl.com/pbbzjv7 - link to Wikipedia, paragraph 3.
@@ -21,7 +22,7 @@ class Series(models.Model):
 
 	#series = models.AutoField(primary_key=True) # id
 	country = models.ForeignKey('Countries')
-	series_name = models.CharField(max_length=100000)
+	series_name = models.CharField(max_length='255')
 
 class Metals(models.Model):
 	class Meta():
@@ -41,13 +42,21 @@ class Mints(models.Model):
 	mint_abbreviation = models.CharField(max_length=10)
 
 
-class Denominals(models.Model):
-	''' "ruble, dollar" for example ''' 
-	class Meta:
-		db_table = 'Denominals'
+#class Denominals(models.Model):
+#	''' "ruble, dollar" for example '''
+#	class Meta:
+#		db_table = 'Denominals'
+#
+#	denominal_name = models.CharField(max_length='50')
+#	denominal_country = models.ForeignKey('Countries')
 
-	denominal_name = models.CharField(max_length='50')
-	denominal_country = models.ForeignKey('Countries')
+# Качество чеканки монет
+class Qualities(models.Model):
+	class Meta:
+		db_table = 'Qualities'
+	quality_coinage = models.CharField(max_length='30')
+	quality_abbr = models.CharField(max_length='10', blank=True)
+	quality_description = models.CharField(max_length='255')
 
 
 class Coins(models.Model):
@@ -58,26 +67,31 @@ class Coins(models.Model):
 	country = models.ForeignKey('Countries')
 	series = models.ForeignKey('Series')
 	coin_metal = models.ForeignKey('Metals')
+	quality = models.ForeignKey('Qualities')
 
-	rate = models.IntegerField() #coin rating
-	denominal_name = models.ForeignKey('Denominals') #currency
-
-	manufacture_date = models.DateField()
-	coin_circulation = models.BigIntegerField()
-	coin_weight = models.FloatField()
-	coin_diameter = models.FloatField()
-	coin_thickness = models.FloatField()
-	coin_herd = models.CharField(max_length='255') # edge of a coin http://tinyurl.com/m56ms6y
+	coin_name = models.CharField(max_length='255')
+	link_cbr = models.URLField() # link on description coin on site Central Bank of Russia
+	description_observe = models.CharField(max_length='255')
+	description_reverse = models.CharField(max_length='255')
 	painter = models.CharField(max_length='255')
 	sculptor = models.CharField(max_length='255')
-	description = models.CharField(max_length='255')
+	coin_herd = models.CharField(max_length='255', blank=True) # edge of a coin http://tinyurl.com/m56ms6y
+	photo_obverse = models.ImageField(upload_to='/static/coins')
+	photo_reverse = models.ImageField(upload_to='/static/coins')
+
+	rate = models.IntegerField() #coin rating
+	denominal = models.CharField(max_length='255')
+	#denominal_name = models.ForeignKey('Denominals') #currency
+	coin_weight = models.FloatField(blank=True)
+	chemistry = models.FloatField(blank=True)
+	coin_diameter = models.FloatField(blank=True)
+	coin_thickness = models.FloatField(blank=True)
+	coin_circulation = models.BigIntegerField()
+	manufacture_date = models.DateField()
 	item_number = models.CharField(max_length=9) # http://tinyurl.com/kbh5fly
-	photo_obverse = models.ImageField()
-	photo_reverse = models.ImageField()
-	link_cbr = models.URLField() # link on description coin on site Central Bank of Russia
 
 
-class  CoinToMint(models.Model):
+class CoinToMint(models.Model):
 	class Meta:
 		db_table = 'CoinToMint'
 
