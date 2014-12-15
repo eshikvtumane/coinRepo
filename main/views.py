7dab761d5a09d00d28ecafd80cfa6e83add6912e
eshik <eshikvtumanebest@gmail.com>
2014-12-15 16:36:21 +1100

    Add url for home page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.views.generic import View
+from django.template import RequestContext
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         template = 'main/home.html'
-        return render_to_response(template)
+        return render_to_response(template, RequestContext(request))
 
