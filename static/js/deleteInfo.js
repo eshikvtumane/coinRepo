972dfee3812d53fe4be4ffb0612d775aab23153c
eshik <eshikvtumanebest@gmail.com>
2014-12-30 20:52:12 +1100

    Add delete info about coins

---
+++ static/js/deleteInfo.js
@@ -0,0 +1,19 @@
+function deleteSeries(elem){
+        var series_id = elem.value;
+        console.log(elem.parentNode.rowIndex);
+        $.ajax({
+            type: 'POST',
+            url:'/user/series/delete/',
+            data:{
+                's_id': series_id
+            },
+            success:function(data){
+                if(data == '200'){
+                    $('tr[id="'+series_id+'"]').remove()
+                }
+                else{
+                    console.log('Sheet');
+                }
+            }
+        });
+    }
