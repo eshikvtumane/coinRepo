b5a068eceb5a18dc73eeb44bf6f1e95ef1060146
eshikvtumane <eshikvtumanebest@gmail.com>
2015-01-31 18:41:41 +1000

    add parser for grabbing price from molotok.ru

--- parsers/parcer_prices/MolotokParce.py
+++ parsers/parcer_prices/MolotokParce.py
@@ -7,22 +7,39 @@ class ParcePrice():
 	def __init__(self):
 		self.stop_list = ()
 
-	def get_price(self, obj):
+	def get_price(self, obj, con):
 		date = datetime.date.today()
 		results = ()
+
+		# forming link
+		words = self.get_hashtags(con, obj[0])
 		link = 'http://molotok.ru/listing/listing.php?category=48393&p='
-		link_end = '&search_scope=category-48393&string=%s+%s+%s' % (obj[2], obj[3], obj[1].replace(' ', '+'))
+		link_end = '&search_scope=category-48393&string=%s+%s'% (obj[2], obj[3])
+
+		if len(words) != 0:
+			query = ''
+			for word in words:
+				query += '+' + word[0]
+			
+			link_end += query
+		else:
+			#link_end = '&search_scope=category-48393&string=%s+%s+%s'
+			link_end += '+' + obj[1].replace(' ', '+')
+
 
 		count_page = 1
 		buf_result = []
+
 		while(True):
 			res = []
 			while(True):
 				try:
-					res = self.parse_prices(link + str(count_page) + link_end)
+					l = link + str(count_page) + link_end
+					res = self.parse_prices(l)
 					break
 				except:
 					print 'Error connect. Retry ...'
+
 			if res == []:
 				break
 			if count_page > 5:
@@ -33,11 +50,12 @@ class ParcePrice():
 		if buf_result != []:
 			results = (obj[0], self.get_median(buf_result), '%s1%s'%(link, link_end), date, )
 			return results
-		
+
 		return False
 
 	def parse_prices(self, link):
 		response = urllib2.urlopen(link)
+
 		html = response.read()
 		#print link
 		page = BS(html)
@@ -49,6 +67,7 @@ class ParcePrice():
 				price = re.findall('\d+.\d+', obj.get_text())
 				arr_price = [ float(p.replace(',', '.').replace(' ', '')) for p in price ]
 				array_prices += arr_price
+
 		return array_prices
 
 	def get_median(self, arr):
@@ -58,3 +77,10 @@ class ParcePrice():
 		if length_arr % 2 == 0:
 			return (arr[(length_arr / 2) - 1] + arr[(length_arr / 2)]) / 2
 		return arr[int(length_arr / 2)]
+
+	def get_hashtags(self, con, id):
+		sql = 'SELECT search_word FROM CoinSearchWords WHERE coin_id = %s' % id
+		cur = con.cursor()
+		cur.execute(sql)
+		select = cur.fetchall()
+		return select
--- parsers/parcer_prices/get_price.py
+++ parsers/parcer_prices/get_price.py
@@ -28,7 +28,7 @@ def get_coins(con):
 
 def get_price(obj):
 	price = ParcePrice()
-	price_obj = price.get_price(obj)
+	price_obj = price.get_price(obj, con)
 	print obj[1]
 	return price_obj
 		
@@ -41,9 +41,10 @@ def insert_data(con, data):
 	con.executemany(sql_insert, data)
 	con.commit()
 
+print 'Connect DB ...'
+con = connect_db()
+
 if __name__ == '__main__':
-	print 'Connect DB ...'
-	con = connect_db()
 	print 'Get coins ...'
 	coins = get_coins(con)
 	print 'Get prices ...'
@@ -51,11 +52,6 @@ if __name__ == '__main__':
 
 	result = pool.map(get_price, coins)
 
-	'''result = []
-	for obj in coins[:5]:
-		p = mp.Process(target=get_price, args = (obj,))
-		result.append(p)
-		p.start()'''
 
 	while False in result:
 		result.remove(False)
