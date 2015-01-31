b4ec135c6a06d33e63d5a0db52cb47add13eb84f
eshikvtumane <eshikvtumanebest@gmail.com>
2015-01-31 18:48:41 +1000

    add change in main page and header

--- main/views.py
+++ main/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, render_to_response
 from django.views.generic import View
 from django.template import RequestContext
 
+from news.models import News
+
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         template = 'main/home.html'
-        return render_to_response(template, RequestContext(request))
+        args = {}
+        news = list(News.objects.filter().order_by('-pub_date')[:10])
+        args['news'] = news
+        return render_to_response(template, RequestContext(request, args))
 
