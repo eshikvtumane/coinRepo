f3c13b53114fc283a0fa553ce797d2645f6168cd
eshikvtumane <eshikvtumanebest@gmail.com>
2015-01-31 18:45:59 +1000

    add model in settings, index whoosh

--- coins_site/settings.py
+++ coins_site/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = (
     'main',
     'coins',
     'users',
+    'news',
+    'shop',
 )
 
 MIDDLEWARE_CLASSES = (
--- coins_site/urls.py
+++ coins_site/urls.py
@@ -14,6 +14,10 @@ urlpatterns = patterns('',
 
 
     url(r'^user/', include('users.urls')),
+    url(r'^news', include('news.urls')),
+
+    url(r'shop/', include('shop.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include('haystack.urls')),
 )
